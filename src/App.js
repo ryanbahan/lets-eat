2af3b1efc94429b3cd637a0665c61c080276ef27
Ryan Bahan <ryan@ryanbahan.com>
2020-04-28 14:19:46 -0600

    Initial commit w/ fetch test

--- src/App.js
+++ src/App.js
@@ -3,22 +3,16 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  function test() {
+    fetch('https://code-challenge.spectrumtoolbox.com/api/restaurants', { headers: {
+      Authorization: 'Api-Key q3MNxtfep8Gt', },
+    }).then(res => res.json()).then(data => console.log(data))
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    {test()}
     </div>
   );
 }
