46a69c594216ccc91e1c0949938badecaf302abc
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-30 20:31:15 +0000

    Deploy ryanbahan/lets-eat to github.com/ryanbahan/lets-eat.git:gh-pages

---
+++ precache-manifest.b2612478c69790cbe3f08bbb7c35ea79.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63289c8bc8e4e4225b4a4aefc7d636c1",
+    "url": "/lets-eat/index.html"
+  },
+  {
+    "revision": "25e3e2b46502ff1e749f",
+    "url": "/lets-eat/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f0b8767d886f9b92867c",
+    "url": "/lets-eat/static/js/2.c94bdf0f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lets-eat/static/js/2.c94bdf0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25e3e2b46502ff1e749f",
+    "url": "/lets-eat/static/js/main.fb23fe07.chunk.js"
+  },
+  {
+    "revision": "1c1815bd6a95680f6878",
+    "url": "/lets-eat/static/js/runtime-main.1de13f13.js"
+  }
+]);
